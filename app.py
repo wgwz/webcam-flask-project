c18642a7f5805abadcba7fec933688cbbef4855d
wgwz <klawlor419@gmail.com>
2017-05-26 16:57:36 -0400

    init

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from flask_uploads import configure_uploads, UploadSet, IMAGES
+
+app = Flask(__name__)
+app.config.update({
+    'UPLOADS_DEFAULT_DEST': '/tmp/uploads',
+    'UPLOADS_DEFAULT_URL': 'http://localhost:5000'
+})
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        return 'uploaded'
+    return "nothing to see here, move along."
